5efcd55b55f5653bd5d777bc4bad291dc3e5ab58
magrita00 <sr.hong0114@gmail.com>
2023-05-01 19:21:21 +0900

    first commit

---
+++ ch02/src/sec02/exam07/FloatDoubleExample.java
@@ -0,0 +1,17 @@
+package sec02.exam07;
+
+public class FloatDoubleExample {
+	public static void main(String[] args) {
+		// float var1 = 3.14;
+		float var2=3.14f;
+		double var3=3.14;
+		
+		float var4=0.1234567890123456789f;
+		double var5=0.1234567890123456789;
+		
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+}
